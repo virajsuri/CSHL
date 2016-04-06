3a6d18834b6e71a64bc99b0f057640f0eaec6f04
virajsuri <vpsuri@irusmail.com>
2016-04-05 21:52:53 -0400

    add util gitignore

--- src/normalWork/Test.java
+++ src/normalWork/Test.java
@@ -3,6 +3,6 @@ import viraj.util.*;
 
 public class Test {
 	public static void main(String [] args) {
-		Encryption.encryptString("Hello world", "ANT&$*459:'[qwer");
+		System.out.println(viraj.util.Maths.randomIntBetween(3, 10));
 	}
 }
